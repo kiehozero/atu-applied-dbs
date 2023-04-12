28b4ce6fe0cd2de4397c4109556cc60ff134ebf6
Stuart Saint <stuartsaint242@gmail.com>
2023-04-12 21:26:18 +0100

    Start of lecture

--- wk10/school.py
+++ wk10/school.py
@@ -1,3 +1,16 @@
-import pymysql as mysql
+import pymysql.cursors
 
-mysql.connect("localhost", "root", "root", "school")
+conn = pymysql.connect(host='localhost', user='root', password='root', database='school', cursorclass=pymysql.cursors.DictCursor)
+
+# %s can be used to insert values
+
+query = 'SELECT * FROM subject WHERE teacher LIKE %s'
+
+ins = 'INSERT INTO subject(Name, Teacher, OnLeavingCert) VALUES (%s, %s, %s)'
+
+with conn:
+    cursor = conn.cursor()
+    cursor.execute(query, ('Ms.%'))
+    subjects = cursor.fetchall()
+    for s in subjects:
+        print(s['Name'])
